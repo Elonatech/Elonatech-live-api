87efdb134e0023fb87aa38d1d5c8bceaf92f6155
Elonatech <support@elonatech.com.ng>
2025-10-31 13:48:47 +0100

    my second commit

--- routes/visitorRoutes.js
+++ routes/visitorRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const visitorController = require("../controller/VisitorController");
+const visitorController = require("../controller/visitorController");
 
 router.get("/monthly", visitorController.getMonthlyVisitors);
 
